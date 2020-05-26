a05be2041ade346ef0e7ca20f3d7cd6be7de7251
Babu <babula38@gmail.com>
2020-05-26 09:07:35 +0530

    Fixed some issues Reamane the file for wpp.property

---
+++ WebApplication1/WebForm2.aspx.b.cs
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebApplication1.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form runat="server" id="main">
+    </form>
+    <div id="root"></div>
+    <script>
+        window.baseUrl = '<%= ResolveUrl ("~") %>';
+    </script>
+    <script src="React/dist/parts.bundle.js" type="text/javascript"></script>
+</body>
+</html>
